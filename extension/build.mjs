d73c6b1c0ecacc3f4305d408af256fd99f5dc127
Ray Wan <wanchr@gmail.com>
2026-09-23 02:00:41 +0800

    Bump to v0.3.0 (companion v1.0.5)

--- extension/build.mjs
+++ extension/build.mjs
@@ -21,7 +21,7 @@ export async function build(output = root) {
   const common = {
     manifest_version: 3,
     name: 'BrowserDock Companion',
-    version: '1.0.4',
+    version: '1.0.5',
     description: 'Connect this browser to BrowserDock on your computer to focus or open tabs.',
     permissions: ['tabs', 'storage', 'alarms', 'tabGroups'],
     host_permissions: ['http://127.0.0.1/*'],
